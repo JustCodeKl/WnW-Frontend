b3de91ce9dfa41f5491de5590d677d193ea99eb8
Willy Wakam <williamswakam09@gmail.com>
2024-11-24 09:24:12 +0100

    Refactoring

--- src/pages/PlacePage.jsx
+++ src/pages/PlacePage.jsx
@@ -129,7 +129,7 @@ export default function PlacePage() {
             d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
           />
         </svg>
-        {newAddress}
+        {newAddress !== undefined && newAddress}
       </a>
       <div className="grid grid-cols-2 h-[800px] w-full mt-2 gap-2 mb-10 max-sm:h-[250px]">
         <div className="">
