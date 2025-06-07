b0fa46bb18ca1227d532d1674470e0acb4043056
Willy Wakam <williamswakam09@gmail.com>
2025-06-07 17:53:49 +0200

    Add some imports.

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { fadeIn } from "react-animations";
+import styled, { keyframes } from "styled-components";
 
 export default function IndexPage(){
     const [allPlaces, setAllPlaces] = useState([]);
