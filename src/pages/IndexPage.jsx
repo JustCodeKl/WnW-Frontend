71f7f3cba10e517c93fdc8c14237a39bc53dca1e
Willy Wakam <williamswakam09@gmail.com>
2025-06-07 18:02:49 +0200

    Update IndexPage.jsx

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { fadeIn } from "react-animations";
-import styled, { keyframes } from "styled-components";
 
 export default function IndexPage(){
     const [allPlaces, setAllPlaces] = useState([]);
