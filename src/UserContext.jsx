f9bafeeea771e9b41e897c15d99ff130f013f6e7
Willy Wakam <williamswakam09@gmail.com>
2025-06-08 16:37:51 +0200

    Fix bug

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -39,7 +39,7 @@ export function UserContextProvider({children}){
               });
         }
         
-    }, [logout, user, usersList])
+    }, [user, usersList])
 
     return (
        <UserContext.Provider value={{user, setUser, ready, logout, setLogout, redirect, setRedirect, usersList, addedPlacesList, filterPlaces, setFilterPlaces}}>
