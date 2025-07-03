3c1ea6b9397bc4d586cc9233380704705b53dcc0
Willy Wakam <williamswakam09@gmail.com>
2025-07-04 00:54:04 +0200

    Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -63,7 +63,7 @@ export default function Header() {
             {
               !user && (
                 <div role='username'>
-                  {user.name}
+                  {user?.username}
                 </div>
               )
             }
